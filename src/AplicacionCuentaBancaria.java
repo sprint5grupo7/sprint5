789209ed3d437108a6dc51673ce37ce0197b0536
Antonio Miguel Cobos Fernandez <cobosmoriles@gmail.com>
2018-04-07 23:43:29 +0200

    Sustitución de un bloque de código por un metodo

--- src/AplicacionCuentaBancaria.java
+++ src/AplicacionCuentaBancaria.java
@@ -32,17 +32,7 @@ public class AplicacionCuentaBancaria {
         
         do {
 
-            System.out.println("Elige una opcion: ");
-            System.out.println("1. Ver el número de cuenta completo(CCC).");
-            System.out.println("2. Ver el titular de la cuenta.");
-            System.out.println("3. Ver el código de la entidad.");
-            System.out.println("4. Ver el código de la oficina.");
-            System.out.println("5. Ver el número de la cuenta.");
-            System.out.println("6. Ver los dígitos de control de la cuenta.");
-            System.out.println("7. Realizar un ingreso.");
-            System.out.println("8. Retirar efectivo.");
-            System.out.println("9. Consultar saldo.");
-            System.out.println("10.Salir de la aplicación.");
+                imprimirMenu();
 
             do {
                 Scanner sc = new Scanner(System.in);
@@ -142,6 +132,20 @@ public class AplicacionCuentaBancaria {
             System.err.println("Ha ocurrido algun error");
         }
         
+    }
+
+    private static void imprimirMenu() {
+        System.out.println("Elige una opcion: ");
+        System.out.println("1. Ver el número de cuenta completo(CCC).");
+        System.out.println("2. Ver el titular de la cuenta.");
+        System.out.println("3. Ver el código de la entidad.");
+        System.out.println("4. Ver el código de la oficina.");
+        System.out.println("5. Ver el número de la cuenta.");
+        System.out.println("6. Ver los dígitos de control de la cuenta.");
+        System.out.println("7. Realizar un ingreso.");
+        System.out.println("8. Retirar efectivo.");
+        System.out.println("9. Consultar saldo.");
+        System.out.println("10.Salir de la aplicación.");
     }
 
 }
